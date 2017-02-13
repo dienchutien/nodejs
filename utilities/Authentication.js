33dc019774c4a3d0c1e178a597f95d54e4e676f9
Chu Tien Dien <dienctsimplelife@gmail.com>
2017-02-13 16:55:54 +0700

    new

--- utilities/Authentication.js
+++ utilities/Authentication.js
@@ -1,6 +1,6 @@
 exports.Authenticate = function (request, response, next) {
-    if (Object.keys(request.cookies).length > 0) {
-        //console.log(Object.keys(request.cookies).length);
+    if (request.cookies.tencookie != undefined && request.cookies.tencookie !== null) {
+        //console.log(request.cookies.tencookie);
         return next();
     } else {
         response.redirect('/admin/login');
